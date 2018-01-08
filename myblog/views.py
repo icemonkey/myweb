2c1bdb9fef6f94d46aeb316eb6bf6de84e750c17
icemonkey <874916881@qq.com>
2018-01-08 18:04:26 +0800

    update list

--- myblog/views.py
+++ myblog/views.py
@@ -15,6 +15,12 @@ def post(request,year,mon,day,post_num):
     return render_to_response('myblog/post.html',quote)
 
 def listdate(request,list_date):
+    list_date1 = '{}'.format(list_date)
     list_date = 'Listdate is {}'.format(list_date)
-    quote = {'quote':list_date}
-    return render_to_response('myblog/post.html',quote)
+    
+    b = []
+    [b.append(int(n)) for n in list_date1.split('/')]
+    total = sum(b)
+    val = {'quote':list_date,'total':total}
+    print total
+    return render_to_response('myblog/post.html',val)
