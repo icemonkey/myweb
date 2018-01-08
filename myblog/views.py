62f75280012ecf82a91abe42ab1f3c78154dad29
icemonkey <874916881@qq.com>
2018-01-08 18:39:46 +0800

    update list

--- myblog/views.py
+++ myblog/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def about(request):
@@ -21,6 +22,11 @@ def listdate(request,list_date):
     b = []
     [b.append(int(n)) for n in list_date1.split('/')]
     total = sum(b)
-    val = {'quote':list_date,'total':total}
+    now = datetime.now()
+    
+    hour = now.timetuple().tm_hour
+    val = {'quote':list_date,'total':total,'now':now,'hour':hour}
+
     print total
+    print hour
     return render_to_response('myblog/post.html',val)
